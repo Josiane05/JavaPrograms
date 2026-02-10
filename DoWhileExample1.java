cfb398dd356f11699264a2a94e99131cf1171b09
Josiane05 <josianemushimiyimana05@gmail.com>
2026-02-10 13:40:48 +0200

    DoWhile

---
+++ DoWhileExample1.java
@@ -0,0 +1,10 @@
+public class DoWhileExample1 {
+    public static void main(String[] args) {
+        int i = 1;
+
+        do { 
+            System.err.println(i);
+            i++;
+        } while(i<=5);
+    }
+}
