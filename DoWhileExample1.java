43f14a9373aafa92ede51567be8899c4a7d15981
Josiane05 <josianemushimiyimana05@gmail.com>
2026-02-10 13:58:30 +0200

    For loop

--- DoWhileExample1.java
+++ DoWhileExample1.java
@@ -2,7 +2,7 @@ public class DoWhileExample1 {
     public static void main(String[] args) {
         int i = 1;
 
-        do { 
+        do {
             System.err.println(i);
             i++;
         } while(i<=5);
---
+++ ForTable.java
@@ -0,0 +1,11 @@
+public class ForTable {
+    public static void main(String[] args) {
+        int n;
+        int x=5;
+        for(n=1;n<=10;n++)
+        {
+            int p = x*n;
+            System.err.println(x+"*"+n+"="+p);
+        }
+    }
+}
