6e755777543f2df5b5585858f10da426f156839c
Josiane05 <josianemushimiyimana05@gmail.com>
2026-02-10 14:28:10 +0200

    loopchk

---
+++ Loopchk.java
@@ -0,0 +1,6 @@
+public class Loopchk {
+    public static void main(String[] args) {
+        for(int i=1, j=5;i>0&&j>2;i++,j--)
+            System.out.println("i is: "+i+"and j is:"+j);
+    }
+}
---
+++ var2.java
@@ -0,0 +1,11 @@
+public class var2 {
+    public static void main(String[] args) {
+        int a, b;
+        b = 5;
+        for(a=0; a<b; a++) {
+            System.out.println("a=" +a);
+            System.out.println("b=" +b);
+            b--;
+        }
+    }
+}
